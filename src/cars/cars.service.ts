511b617aaa9bb9da90b64aee4a859622a30bf53e
eleo romario <eleo.romario@gmail.com>
2024-07-16 09:08:16 -0500

    chore: Add uuid dependency and update code to use UUIDs instead of numbers

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,22 +1,22 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { Car } from './interfaces/car.interface';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class CarsService {
-  private cars = [
+  private cars: Car[] = [
     {
-      id: 1,
+      id: uuid(),
       make: 'Toyota',
       model: 'Corolla',
-      year: 2019,
     },
     {
-      id: 2,
+      id: uuid(),
       make: 'Honda',
       model: 'Accord',
-      year: 2018,
     },
     {
-      id: 3,
+      id: uuid(),
       make: 'Ford',
       model: 'Fusion',
     },
@@ -26,7 +26,7 @@ export class CarsService {
     return this.cars;
   }
 
-  findOneById(id: number) {
+  findOneById(id: string) {
     const car = this.cars.find((car) => car.id === id);
 
     if (!car) throw new NotFoundException(`Car with ID ${id} not found`);
